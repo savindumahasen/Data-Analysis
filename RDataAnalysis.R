e550254a041f38b35643646d5bc5225ee5c2b5d5
Savindu <savinduruhunuhewa@gmail.com>
2024-01-21 20:55:08 +0530

    bind the data set with R environment

--- RDataAnalysis.R
+++ RDataAnalysis.R
@@ -5,5 +5,10 @@ energy_effieciency_data <- read.csv("energy_efficiency_data.csv");
 View(energy_effieciency_data)
 str(energy_effieciency_data)
 
+### bind the dataset with R enviornment
+attach(energy_effieciency_data);
+View(energy_effieciency_data);
 
-### 
+### checking the null  values ###
+
+
