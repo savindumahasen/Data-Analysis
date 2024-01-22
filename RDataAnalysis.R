2effd28a5c55d1e2441c17ad3dac053d87aed606
Savindu <savinduruhunuhewa@gmail.com>
2024-01-22 11:53:44 +0530

    complete the normality testing

--- RDataAnalysis.R
+++ RDataAnalysis.R
@@ -12,10 +12,21 @@ View(energy_effieciency_data);
 ### checking the null  values ###
 energy_effieciency_data[!complete.cases(energy_effieciency_data),]
 
+### summary about the data set ###
+summary(energy_effieciency_data);
 
 #### Normality  Testing ####
 ### Install nortest package ###
 install.packages("nortest");
 ### calling the library ###
 library("nortest");
-###
+### Normality Testing ###
+
+## Anderson Darling testing ##
+ad.test(Cooling_Load);
+
+## Lillifor Testing ##
+lillie.test(Cooling_Load);
+
+## shapiro-wilk testing ##
+shapiro.test(Cooling_Load)
