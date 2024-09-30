95505fff30fe6c9521b31ce4f3a369d5b6ef9488
Savindu <savinduruhunuhewa@gmail.com>
2024-09-30 09:04:32 +0530

    Complete the normality testing and drwa bell curves

--- RDataAnalysis.R
+++ RDataAnalysis.R
@@ -25,6 +25,101 @@ install.packages("nortest")
 ## call the library
 library("nortest")
 
+## Relative_Compactness
+ad.test(Relative_Compactness)
+lillie.test(Relative_Compactness)
+shapiro.test(Relative_Compactness)
+
+## Histogram and bell curve for Relative_Compactness
+hist(Relative_Compactness, main="Relative_Compactness distribution", xlab ="Relative_Compactness Category", ylab="Relative_Compactness",
+     prob=TRUE)
+
+curve(dnorm(x, mean=mean(Relative_Compactness, na.rm=TRUE), sd=sd(Relative_Compactness, na.rm = TRUE)), add=TRUE)
+
+## Surface_Area
+ad.test(Surface_Area)
+lillie.test(Surface_Area)
+shapiro.test(Surface_Area)
+
+## Histogram and bell curve for Surface_Area
+hist(Surface_Area, main = "Surface_Area Distribution", xlab="Surface_Area Category", ylab="Surface_Area", prob=TRUE)
+curve(dnorm(x, mean=mean(Surface_Area, na.rm = TRUE), sd=sd(Surface_Area, na.rm = TRUE)), add=TRUE)
+
+## Wall_Area
+ad.test(Wall_Area)
+lillie.test(Wall_Area)
+shapiro.test(Wall_Area)
+
+## Histogram and bell curve for Wall_Area
+hist(Wall_Area, main="Wall_Area Distribution", xlab="Wall_Area Category", ylab="Wall_Area", prob=TRUE)
+curve(dnorm(x, mean=mean(Wall_Area, na.rm=TRUE), sd=sd(Wall_Area, na.rm = TRUE)), add=TRUE)
+
+## Roof_Area
+ad.test(Roof_Area)
+lillie.test(Roof_Area)
+shapiro.test(Roof_Area)
+
+## Histogram and bell curve for Roof_Area
+hist(Roof_Area, main="Roof_Area Distribution", xlab = "Roof_Area Category", ylab="Roof_Area", prob=TRUE)
+curve(dnorm(x, mean=mean(Roof_Area, na.rm=TRUE), sd=sd(Roof_Area, na.rm = TRUE)), add=TRUE)
+
+## Overall_Height
+ad.test(Overall_Height)
+lillie.test(Overall_Height)
+shapiro.test(Overall_Height)
+
+## Histogram and bell curve for Overall_Height
+hist(Overall_Height, main="Overall_Height Distribution", xlab = "Overall_Height Category", ylab="Overall_Height",
+     prob=TRUE)
+curve(dnorm(x, mean=mean(Overall_Height, na.rm=TRUE), sd=sd(Overall_Height, na.rm = TRUE)), add=TRUE)
+
+## Orientation
+ad.test(Orientation)
+lillie.test(Orientation)
+shapiro.test(Orientation)
+
+## Histogram and bell curve for Orientation
+hist(Orientation, main="Orientation Distribution",xlab="Orientation Category", ylab="Orientation", prob=TRUE)
+curve(dnorm(x, mean=mean(Orientation, na.rm = TRUE), sd=sd(Orientation, na.rm=TRUE)), add=TRUE)
+
+## Glazing_Area
+ad.test(Glazing_Area)
+shapiro.test(Glazing_Area)
+lillie.test(Glazing_Area)
+
+## Histogram and bell curve for Glazing_Area
+hist(Glazing_Area, main="Glazing_Area Distribution", xlab="Glazing_Area Category", ylab="Glazing_Area", prob=TRUE)
+curve(dnorm(x, mean=mean(Glazing_Area, na.rm = TRUE), sd=sd(Glazing_Area, na.rm = TRUE)), add=TRUE)
+
+## Glazing_Area_Distribution
+ad.test(Glazing_Area_Distribution)
+lillie.test(Glazing_Area_Distribution)
+shapiro.test(Glazing_Area_Distribution)
+
+## Histogram and bell curve for Glazing_Area_Distribution
+hist(Glazing_Area_Distribution, main="Glazing_Area_Distribution", xlab="Glazing_Area_Distribution Category", ylab="Glazing_Area_Distribution",
+     prob=TRUE)
+curve(dnorm(x, mean=mean(Glazing_Area_Distribution, na.rm = TRUE), sd=sd(Glazing_Area_Distribution, na.rm=TRUE)), add=TRUE)
+
+## Heating_Load
+ad.test(Heating_Load)
+lillie.test(Heating_Load)
+shapiro.test(Heating_Load)
+
+## Histogram and bell curve for Heating_Load
+
+hist(Heating_Load, main="Heating_Load Distribution", xlab="Heating_Load Category", ylab="Heating_Load", prob=TRUE)
+curve(dnorm(x, mean=mean(Heating_Load, na.rm=TRUE), sd=sd(Heating_Load, na.rm = TRUE)), add=TRUE)
+
+## Cooling_Load
+ad.test(Cooling_Load)
+lillie.test(Cooling_Load)
+shapiro.test(Cooling_Load)
+
+## Histogram and bell curve for Cooling_Load
+hist(Cooling_Load, main="Cooling_Load Distribution", xlab = "Cooling_Load Category", ylab="Cooling_Load", prob=TRUE)
+curve(dnorm(x, mean=mean(Cooling_Load, na.rm=TRUE), sd=sd(Cooling_Load, na.rm=TRUE)),add=TRUE)
+
 
 ### correlational Analysis
 
